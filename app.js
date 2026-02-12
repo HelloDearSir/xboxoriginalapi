b4d58e7ef5cb32c801bc10d53c38f04dfd65d116
HelloDearSir <jonbarton@hotmail.co.uk>
2026-02-12 23:12:31 +0000

    api testing fun

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req,res) => {
+    res.send("Hello World");
+})
+
+let posts = [
+    { id: 1, title: "post 1"},
+    {id:2, title:"posts 2 "}
+];
+const port = 5000;
+app.get("/posts", (req,res) => {
+    res.json(posts);
+})
+
+app.listen(port,() => {
+    console.log(`Server is running on port ${port}`);
+})

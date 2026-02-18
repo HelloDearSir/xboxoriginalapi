ed92604040ac953d2a4cf1d6a1df8f3c6709d274
HelloDearSir <jonbarton@hotmail.co.uk>
2026-02-18 23:58:28 +0000

    adding homepage for the api

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
 app.use(express.json());
 const xboxgames = require('./routers/xboxgames.js')
 app.get('/', (req,res) => {
-    res.send("Hello World");
+    res.sendFile(path.join(__dirname, '/html/index.html'))
 })
 
-
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; connect-src 'self'  https://xboxoriginalapi-production.up.railway.app"
+  );
+  next();
+});
 const port = 5000;
 app.get("/posts", (req,res) => {
     res.json(posts);
